9ec874550683636dc58df0b2dd6db654b7c1dc27
petaretjahuu <157755378+petaretjahuu@users.noreply.github.com>
2024-03-22 21:55:04 +0100

    Update centos_user.sh

--- Timelapse/centos_user.sh
+++ Timelapse/centos_user.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Add user "timelapse" and add it to the "wheel" group with sudo privileges
-sudo useradd -m -p $(openssl passwd -1 timelapse) timelapse && sudo usermod -aG wheel timelapse
+sudo useradd timelapse && echo "timelapse:timelapse" | sudo chpasswd && sudo usermod -aG wheel timelapse
